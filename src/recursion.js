337bd9f05478be9e9d1c432fa347eed9e37ff4cf
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-04-20 23:14:34 -0700

    Solve augmentElements function.

--- src/recursion.js
+++ src/recursion.js
@@ -468,12 +468,21 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+
 };
 
 // 33. Augument every element in a list with a new value where each element is an array
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+  if (array.length === 1) {
+    array[0].push(aug);
+    return array;
+  }
+  let resultArr = augmentElements(array.slice(1), aug);
+  array[0].push(aug);
+  resultArr.unshift(array[0]);
+  return resultArr;
 };
 
 // 34. Reduce a series of zeroes to a single 0.
